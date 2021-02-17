6d7f012123b71cbd8c566eda8c54efca996d1695
Alex K <askrasn@gmail.com>
2021-02-17 07:45:29 -0800

    + getStaticPaths for article fetching

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,13 +1,53 @@
-import {useRouter} from 'next/router'
-const article = (props) => {
-    const router = useRouter();
-    const { id } = router.query;
-    console.log(props)
-    return (
-        <h1>
-            This is an article {id}
-        </h1>
-    )
-}
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
-export default article
+const article = ({ article }) => {
+  // const router = useRouter();
+  // const { id } = router.query;
+
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+      <Link href='/'>Back</Link>
+    </>
+  );
+};
+
+// export const getServerSideProps = async (ctx) => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts/${ctx.params.id}`
+//   );
+//   const article = await res.json();
+//   console.log('AA:', article);
+//   return {
+//     props: {
+//       article,
+//     },
+//   };
+// };
+export const getStaticProps = async (ctx) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${ctx.params.id}`
+  );
+  const article = await res.json();
+  return {
+    props: {
+      article,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`);
+  const articles = await res.json();
+  const ids = articles.map((article) => article.id);
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+  return {
+      paths,
+      fallback: false
+  };
+};
+
+export default article;
